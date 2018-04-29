3a9fff1b964045dd9df8a9cba09a87639bacd42f
ekszodiac <Ekszodiac@gmail.com>
2018-04-29 00:57:03 -0800

    Second Commit ni RVO. Naa ang camera, nagatry pako capture ug frame, partial pa ang naa diria. Ask more details on FB

--- app/src/main/java/com/example/ekszodiac/itp/MainActivity.java
+++ app/src/main/java/com/example/ekszodiac/itp/MainActivity.java
@@ -1,10 +1,19 @@
 package com.example.ekszodiac.itp;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.hardware.Camera;
+import android.media.MediaRecorder;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.SurfaceView;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
@@ -16,9 +25,19 @@ import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.imgproc.Imgproc;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE;
+
 public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
 
     private static final String TAG = "MainActivity";
+    static final int REQUEST_IMAGE_CAPTURE = 1;
     JavaCameraView javacameraview;
     Mat mRgba,imgGray, imgCanny;
     BaseLoaderCallback mLoaderCallBack = new BaseLoaderCallback(this) {
@@ -26,7 +45,6 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         public void onManagerConnected(int status) {
             switch(status){
                 case BaseLoaderCallback.SUCCESS:{
-
                     javacameraview.enableView();
                     break;
                 }
@@ -37,10 +55,6 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
             }
         }
     };
-
-    static {
-
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,6 +72,7 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         if(javacameraview != null) {
             javacameraview.disableView();
         }
+        releaseCamera();
     }
 
     @Override
@@ -105,4 +120,90 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
 
         return mRgbaT;
     }
+
+    //Capture Pictures
+    private Camera.PictureCallback mPicture = new Camera.PictureCallback() {
+
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+
+            File pictureFile = getOutputMediaFile(MEDIA_TYPE_IMAGE);
+            if (pictureFile == null){
+                Log.d(TAG, "Error creating media file, check storage permissions: " +
+                        e.getMessage());
+                return;
+            }
+
+            try {
+                FileOutputStream fos = new FileOutputStream(pictureFile);
+                fos.write(data);
+                fos.close();
+            } catch (FileNotFoundException e) {
+                Log.d(TAG, "File not found: " + e.getMessage());
+            } catch (IOException e) {
+                Log.d(TAG, "Error accessing file: " + e.getMessage());
+            }
+        }
+    };
+
+    // Add a listener to the Capture button
+    Button FScaptureButton = (Button) findViewById(org.opencv.R.id.captureBut);
+    FScaptureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                }
+            }
+    });
+
+
+    //Release Camera
+    private void releaseCamera(){
+        if (javacameraview != null){
+            javacameraview.disableView();        // release the camera for other applications
+            javacameraview = null;
+        }
+    }
+
+    //Save Files
+    public static final int MEDIA_TYPE_IMAGE = 1;
+
+    /** Create a file Uri for saving an image or video */
+    private static Uri getOutputMediaFileUri(int type){
+        return Uri.fromFile(getOutputMediaFile(type));
+    }
+
+    /** Create a File for saving an image or video */
+    private static File getOutputMediaFile(int type){
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
+
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "MyCameraApp");
+        // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
+
+        // Create the storage directory if it does not exist
+        if (! mediaStorageDir.exists()){
+            if (! mediaStorageDir.mkdirs()){
+                Log.d("MyCameraApp", "failed to create directory");
+                return null;
+            }
+        }
+
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE){
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "IMG_"+ timeStamp + ".jpg");
+        } else {
+            return null;
+        }
+
+        return mediaFile;
+    }
+
 }
