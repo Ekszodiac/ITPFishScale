e534b94edf65e608b9f7fc42f9c92a61fcbc5af0
ekszodiac <Ekszodiac@gmail.com>
2018-05-02 22:02:52 -0800

    7th commit padayuna lang

--- app/src/main/java/com/example/ekszodiac/itp/AnalyzeFish.java
+++ app/src/main/java/com/example/ekszodiac/itp/AnalyzeFish.java
@@ -13,6 +13,7 @@ import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.android.Utils;
 import org.opencv.core.Mat;
+import org.opencv.features2d.DescriptorExtractor;
 import org.opencv.features2d.FeatureDetector;
 import org.opencv.highgui.Highgui;
 import org.opencv.imgproc.Imgproc;
@@ -23,11 +24,15 @@ import java.io.File;
 public class AnalyzeFish extends AppCompatActivity {
 
     private static final String TAG = "AnalyzeFish";
+    File capturedImg;
+    Mat capturedMatImg;
     Mat dataMat1;
     Mat dataMat2;
     Mat dataMat3;
     Mat dataMat4;
     ImageView sample;
+    FeatureDetector detector;
+    DescriptorExtractor surfExtractor;
 
     BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
         @Override
@@ -55,10 +60,16 @@ public class AnalyzeFish extends AppCompatActivity {
         sample = (ImageView)findViewById(R.id.sample);
         sample.setImageDrawable(d);
 
-        Bitmap dataset1= BitmapFactory.decodeResource(getResources(),R.drawable.zem);
-        Utils.bitmapToMat(dataset1, dataMat1);
+        //Get file from gallery and change to Bitmap
+        String capImg = "sdcard/fishscale/test.jpg";
+        capturedImg = new File(capImg);
+        capturedMatImg = Highgui.imread(capturedImg.getAbsolutePath(),Highgui.CV_LOAD_IMAGE_COLOR);
+        Bitmap capturedBM = new Bitmap();
+        Utils.matToBitmap(capturedMatImg, capturedBM);
+        capturedBM = capturedBM.copy(Bitmap.Config.ARGB_8888, true);
+        Utils.bitmapToMat(capturedBM, capturedMatImg);
 
-        Imgproc.cvtColor(dataMat1, dataMat1, Imgproc.COLOR_BGR2RGB);
+        compare1(capturedMatImg);
 
 
     }
@@ -66,6 +77,22 @@ public class AnalyzeFish extends AppCompatActivity {
     public void onResume(){
         super.onResume();
         OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_13, this, mLoaderCallback);
+    }
+
+    private int compare1(Mat CapturedMatImage){
+        //Get dataset1 and change from Bitmap to Mat
+        Bitmap dataset1= BitmapFactory.decodeResource(getResources(),R.drawable.zem);
+        dataset1 = dataset1.copy(Bitmap.Config.ARGB_8888, true);
+        dataMat1 = new Mat();
+        Utils.bitmapToMat(dataset1, dataMat1);
+
+        Imgproc.cvtColor(CapturedMatImage, CapturedMatImage, Imgproc.COLOR_BGR2RGB);
+        Imgproc.cvtColor(dataMat1, dataMat1, Imgproc.COLOR_BGR2RGB);
+
+        detector = FeatureDetector.create(FeatureDetector.SURF);
+        surfExtractor = DescriptorExtractor.create(DescriptorExtractor.SURF);
+
+
     }
 
 
