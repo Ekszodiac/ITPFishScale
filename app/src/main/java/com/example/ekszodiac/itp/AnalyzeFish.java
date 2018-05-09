7d8bd25cc42e00d3bb2bd115ae5fbf78ab7b70a7
ekszodiac <Ekszodiac@gmail.com>
2018-05-09 10:39:48 -0800

    10th commit naa ang unt besh patrigger nalang para madebug nato kundi ibutang nalang ang whole class sa kaning mainactivity unya ang analyze fish na button sa main activity nalang ipatrigger

--- app/src/main/java/com/example/ekszodiac/itp/AnalyzeFish.java
+++ app/src/main/java/com/example/ekszodiac/itp/AnalyzeFish.java
@@ -21,9 +21,8 @@ import org.opencv.features2d.DMatch;
 import org.opencv.features2d.DescriptorExtractor;
 import org.opencv.features2d.DescriptorMatcher;
 import org.opencv.features2d.FeatureDetector;
-import org.opencv.features2d.Features2d;
-import org.opencv.highgui.Highgui;
 import org.opencv.imgproc.Imgproc;
+import org.w3c.dom.Text;
 
 import java.io.IOException;
 import java.io.File;
@@ -42,11 +41,8 @@ public class AnalyzeFish extends AppCompatActivity {
     DescriptorExtractor Extractor;
     DescriptorMatcher matcher;
     MatOfDMatch matches;
-    ArrayList matches_final;
-    int min_dist = 500;
+    int dist_limit = 80;
     TextView testTV;
-    List finalMatchesList;
-    Mat mlast;
 
     BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
         @Override
@@ -54,7 +50,7 @@ public class AnalyzeFish extends AppCompatActivity {
             switch (status) {
                 case LoaderCallbackInterface.SUCCESS: {
                     Log.i(TAG, "OpenCV loaded successfully");
-                    compare1();
+
                 }
                 break;
                 default: {
@@ -77,16 +73,27 @@ public class AnalyzeFish extends AppCompatActivity {
         OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_13, this, mLoaderCallback);
     }
 
-    private Mat compare1(){
+    @Override
+    protected void onStart() {
+        super.onStart();
+        testTV = (TextView) findViewById(R.id.testTV);
+        if(compare1() > 0){
+            testTV.setText(compare1());
+        }
+    }
+
+    private int compare1(){
+        List<DMatch> matchList;
+        List<DMatch> matches_final;
+        MatOfDMatch matches_final_mat;
+        List<DMatch> finalMatchesList = new ArrayList<DMatch>();
         try{
             //Get file from gallery and change to Bitmap
             File root = Environment.getExternalStorageDirectory();
             String capImg = root.getAbsolutePath() + "fishscale/test.jpg";
-            capturedImg = new File(capImg);
-            capturedMatImg = Highgui.imread(capturedImg.getAbsolutePath());
-
-            Utils.matToBitmap(capturedMatImg, capturedBM);
-            capturedBM = capturedBM.copy(Bitmap.Config.ARGB_8888, true);
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+            capturedBM = BitmapFactory.decodeFile(capImg, options);
             Utils.bitmapToMat(capturedBM, capturedMatImg);
 
             //Get dataset1 and change from Bitmap to Mat
@@ -99,15 +106,14 @@ public class AnalyzeFish extends AppCompatActivity {
             Imgproc.cvtColor(dataMat1, dataMat1, Imgproc.COLOR_BGR2RGB);
 
             detector = FeatureDetector.create(FeatureDetector.ORB);
-            //surfExtractor = DescriptorExtractor.create(DescriptorExtractor.SURF);
 
             MatOfKeyPoint capturedkeypoints = new MatOfKeyPoint();
             detector.detect(capturedMatImg, capturedkeypoints);
-            Log.d("LOG!",  "number of query Captured Keypoints:" + capturedkeypoints.size());
+            Log.d("FishScale:",  "Captured Keypoints:" + capturedkeypoints.size());
 
             MatOfKeyPoint keypoints = new MatOfKeyPoint();
             detector.detect(dataMat1, keypoints);
-            Log.d("LOG!",  "number of query Keypoints:" + keypoints.size());
+            Log.d("FishScale:",  "Keypoints:" + keypoints.size());
 
 
             Extractor = DescriptorExtractor.create(DescriptorExtractor.ORB);
@@ -115,26 +121,35 @@ public class AnalyzeFish extends AppCompatActivity {
             Mat sourceDescriptors = new Mat();
 
             Extractor.compute(capturedMatImg, capturedkeypoints, capturedDescriptors);
-            Log.d("LOG!", "number of Captured Descriptors:" + capturedDescriptors.size());
+            Log.d("FishScale:", "Captured Descriptors:" + capturedDescriptors.size());
             Extractor.compute(dataMat1, keypoints, sourceDescriptors);
-            Log.d("LOG!", "number of Source Descriptors:" + sourceDescriptors.size());
+            Log.d("FishScale:", "Source Descriptors:" + sourceDescriptors.size());
 
             matcher = DescriptorMatcher.create(DescriptorMatcher.BRUTEFORCE);
             matches = new MatOfDMatch();
             matcher.match(sourceDescriptors,capturedDescriptors,matches);
+            Log.d("FishScale:", "Size:" + matches.size() );
 
-            Features2d.drawMatches(capturedMatImg, capturedkeypoints, dataMat1, keypoints, matches, mlast );
+            matchList = matches.toList();
+            matches_final = new ArrayList<DMatch>();
 
-            testTV = (TextView) findViewById(R.id.testTV);
-            testTV.setText("Bulowk");
+            for(int i = 0; i < matchList.size(); i++){
+                if(matchList.get(i).distance <= dist_limit){
+                    matches_final.add(matches.toList().get(i));
+                }
+            }
 
-            return mlast;
+            matches_final_mat = new MatOfDMatch();
+            matches_final_mat.fromList(matches_final);
+            finalMatchesList = matches_final_mat.toList();
+
+
         }
         catch(Exception e){
             e.printStackTrace();
         }
 
-        return capturedMatImg;
+        return finalMatchesList.size();
 
     }
 
